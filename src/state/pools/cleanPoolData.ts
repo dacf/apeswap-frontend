020d8b0a891b88fe0fcac90f9c6009ae26e66600
Baba Drape <babadrape@gmail.com>
2022-06-10 16:01:39 +0100

    chore: - change json url to reflect branch

--- src/state/pools/cleanPoolData.ts
+++ src/state/pools/cleanPoolData.ts
@@ -5,20 +5,21 @@ import { getPoolApr } from 'utils/apr'
 import { getBalanceNumber } from 'utils/formatBalance'
 
 const fetchPoolTokenStatsAndApr = (pool: PoolConfig, tokenPrices: TokenPrices[], totalStaked, chainId: number) => {
+  console.log('tokenPrices->APR:::', tokenPrices)
   // Get values needed to calculate apr
   const curPool = pool
+  console.log('pool->APR:::', curPool)
   const rewardToken = tokenPrices
     ? tokenPrices.find((token) => pool?.rewardToken && token?.address[chainId] === pool?.rewardToken.address[chainId])
     : pool.rewardToken
+  console.log('rewardToken->APR:::', rewardToken)
   const stakingToken = tokenPrices
     ? tokenPrices.find((token) => token?.address[chainId] === pool?.stakingToken.address[chainId])
     : pool.stakingToken
+  console.log('stakingToken->APR:::', stakingToken)
 
   // Calculate apr
   const apr = getPoolApr(stakingToken?.price, rewardToken?.price, getBalanceNumber(totalStaked), curPool?.tokenPerBlock)
-  console.log('rewardToken->APR:::', rewardToken)
-  console.log('stakingToken->APR:::', stakingToken)
-  console.log('pool->APR:::', pool)
   console.log('apr->APR:::', apr)
   return [stakingToken, rewardToken, apr]
 }
@@ -30,6 +31,8 @@ const cleanPoolData = (
   tokenPrices: TokenPrices[],
   chainId: number,
 ) => {
+  console.log('chunkedPools->cPD:::', chunkedPools)
+  console.log('poolsConfig->cPD:::', poolsConfig)
   const data = chunkedPools.map((chunk, index) => {
     const poolConfig = poolsConfig.find((pool) => pool.sousId === poolIds[index])
     const [startBlock, endBlock, totalStaked] = chunk
@@ -40,9 +43,9 @@ const cleanPoolData = (
       totalStakedFormatted,
       chainId,
     )
-    console.log('rewardToken->CPD:::', rewardToken)
-    console.log('stakingToken->CPD:::', stakingToken)
-    console.log('apr->CPD:::', apr)
+    console.log('rewardToken->cPD:::', rewardToken)
+    console.log('stakingToken->cPD:::', stakingToken)
+    console.log('apr->cPD:::', apr)
     return {
       sousId: poolIds[index],
       startBlock: new BigNumber(startBlock).toJSON(),
--- src/state/pools/fetchPools.ts
+++ src/state/pools/fetchPools.ts
@@ -15,12 +15,10 @@ const fetchPools = async (poolsConfig: PoolConfig[], chainId: number, tokenPrice
   })
   // We do not want the block time for the banana earn banana pool so we append two null values to keep the chunks even
   const vals = await multicall(chainId, [...sousChefABI, ...bananaABI], poolCalls)
+  console.log('vals->fetchPools:::', vals)
   const formattedVals = [null, null, ...vals]
   const chunkSize = formattedVals.length / poolsConfig.length
   const chunkedPools = chunk(formattedVals, chunkSize)
-  console.log('chunkedPools->fetchPools:::', chunkedPools)
-  console.log('poolsConfig->fetchPools:::', poolsConfig)
-  console.log('tokenPrices->fetchPools:::', tokenPrices)
   return cleanPoolData(poolsConfig, poolIds, chunkedPools, tokenPrices, chainId)
 }
 
