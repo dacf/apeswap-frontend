fcc992e34fdc17aeaffd72fe408c83a994000434
defifofum <defifofum0@gmail.com>
2022-06-29 00:00:34 -0400

    feat: Add wallchain api data decoding - Allows verification of the function parameters which the user will be signing

--- src/utils/wallchainService.ts
+++ src/utils/wallchainService.ts
@@ -1,22 +1,100 @@
 import { RouterTypes } from 'config/constants'
 import { WALLCHAIN_PARAMS } from 'config/constants/chains'
-import { Contract } from 'ethers'
+import { BigNumberish, BytesLike, Contract } from 'ethers'
 import { SwapDelay, RouterTypeParams, DataResponse } from 'state/swap/actions'
+import { ApeRouterManager } from '../config/abi/types/ApeRouterManager'
+import ApeRouterManagerABI from '../config/abi/apeRouterManager.json'
 
+/**
+ * Configure whitelisted bonus router functions.
+ *
+ * The data that is brought back from the API will attempt to be decoded by these functions.
+ * If the data cannot be decoded from the allowed functions then the validation SHOULD fail.
+ *
+ * This is important to protect against a compromised API
+ */
+export const ALLOWED_BONUS_ROUTER_FUNCTIONS = <const>[
+  'swapExactTokensForTokens',
+  'swapTokensForExactTokens',
+  'swapExactETHForTokens',
+  'swapTokensForExactETH',
+  'swapExactTokensForETH',
+  'swapETHForExactTokens',
+]
+export type BONUS_ROUTER_FUNCTION_TYPE = typeof ALLOWED_BONUS_ROUTER_FUNCTIONS[number]
+
+interface RouterFunctionParams {
+  amountInMax?: BigNumberish
+  amountIn?: BigNumberish
+  amountOut?: BigNumberish
+  amountOutMin?: BigNumberish
+  path: string[]
+  to: string
+  deadline: BigNumberish
+  router: string
+  masterInput: BytesLike
+}
+
+export function decodeWallchainFunctionData(data: string): {
+  functionName: BONUS_ROUTER_FUNCTION_TYPE | undefined
+  functionParams: RouterFunctionParams | undefined
+} {
+  const apeRouterManager = new Contract(
+    '0x0000000000000000000000000000000000000000',
+    ApeRouterManagerABI,
+  ) as ApeRouterManager
+
+  let functionName
+  let functionParams
+  // eslint-disable-next-line no-restricted-syntax
+  for (const allowedFunctionName of ALLOWED_BONUS_ROUTER_FUNCTIONS) {
+    try {
+      functionParams = apeRouterManager.interface.decodeFunctionData(allowedFunctionName, data)
+      functionName = allowedFunctionName
+      break
+    } catch (e) {
+      //
+    }
+  }
+  return { functionName, functionParams }
+}
+
+/**
+ *
+ * @param dataResponse
+ * @param value
+ * @param account
+ * @param contractAddress
+ * @returns
+ */
 const wallchainResponseIsValid = (
-  dataResonse: DataResponse,
+  dataResponse: DataResponse,
+  methodName: BONUS_ROUTER_FUNCTION_TYPE,
+  args: (string | string[])[],
   value: string,
   account: string,
   contractAddress: string,
 ) => {
-  if (!dataResonse.pathFound) {
+  if (!dataResponse.pathFound) {
     // Opportunity was not found -> response should be ignored -> valid.
     return false
   }
+
+  const { functionName, functionParams } = decodeWallchainFunctionData(dataResponse.transactionArgs.data)
+
+  // TODO: Can validate additional params based on args passed
+  if (!functionParams) {
+    // Function did not decode based on method name passed.
+    return false
+  }
+
+  // TODO: This is where we can send an alert that the arbitrage is not working
   return (
-    dataResonse.transactionArgs.destination.toLowerCase() === contractAddress.toLowerCase() &&
-    dataResonse.transactionArgs.value.toLowerCase() === value.toLowerCase() &&
-    dataResonse.transactionArgs.sender.toLowerCase() === account.toLowerCase()
+    functionParams?.to.toLowerCase() === account.toLowerCase() &&
+    functionName === methodName &&
+    dataResponse.transactionArgs.destination.toLowerCase() === contractAddress.toLowerCase() &&
+    dataResponse.transactionArgs.value.toLowerCase() === value.toLowerCase() &&
+    dataResponse.transactionArgs.sender.toLowerCase() === account.toLowerCase()
   )
 }
 
@@ -32,7 +110,7 @@ const wallchainResponseIsValid = (
  * @param onSetSwapDelay Callback function to set the swap delay state
  */
 export default function callWallchainAPI(
-  methodName: string,
+  methodName: BONUS_ROUTER_FUNCTION_TYPE,
   args: (string | string[])[],
   value: string,
   chainId: number,
@@ -43,7 +121,7 @@ export default function callWallchainAPI(
 ): Promise<any> {
   onSetSwapDelay(SwapDelay.LOADING_ROUTE)
   const encodedData = contract.interface.encodeFunctionData(methodName, args)
-  // If the intiial call fails APE router will be the default router
+  // If the initial call fails APE router will be the default router
   return fetch(`${WALLCHAIN_PARAMS[chainId].apiUrl}?key=${WALLCHAIN_PARAMS[chainId].apiKey}`, {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
@@ -65,9 +143,9 @@ export default function callWallchainAPI(
     })
     .then((responseJson) => {
       if (responseJson) {
-        const dataResonse: DataResponse = responseJson
-        if (wallchainResponseIsValid(dataResonse, value, account, contract.address)) {
-          onBestRoute({ routerType: RouterTypes.BONUS, bonusRouter: dataResonse })
+        const dataResponse: DataResponse = responseJson
+        if (wallchainResponseIsValid(dataResponse, methodName, args, value, account, contract.address)) {
+          onBestRoute({ routerType: RouterTypes.BONUS, bonusRouter: dataResponse })
           onSetSwapDelay(SwapDelay.VALID)
         } else {
           onBestRoute({ routerType: RouterTypes.APE })
