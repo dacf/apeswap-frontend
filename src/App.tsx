a4a4e8dd490e0faddb6299e56fe75fe6b0c7fb62
Obie Dobo <obiedobo@gmail.com>
2023-02-22 19:40:25 -0600

    chore: Axelar

--- src/App.tsx
+++ src/App.tsx
@@ -38,6 +38,23 @@ declare module '@emotion/react' {
   export interface Theme extends ApeSwapTheme {}
 }
 
+const NewSwap = () => {
+  return (
+    <iframe
+      title="squid_widget"
+      width="420"
+      height="700"
+      style={{
+        display: 'block',
+        margin: '20px auto',
+        padding: '10px 0',
+        border: 'none',
+      }}
+      src="https://widget.squidrouter.com/iframe?config=%7B%22companyName%22%3A%22Custom%22%2C%22style%22%3A%7B%22neutralContent%22%3A%22%23888888%22%2C%22baseContent%22%3A%22%23000000%22%2C%22base100%22%3A%22%23FFFFFF%22%2C%22base200%22%3A%22%23F2F2F2%22%2C%22base300%22%3A%22%23E8E8E8%22%2C%22error%22%3A%22%23ED6A5E%22%2C%22warning%22%3A%22%23FFB155%22%2C%22success%22%3A%22%2362C555%22%2C%22primary%22%3A%22%23000000%22%2C%22secondary%22%3A%22%23888888%22%2C%22secondaryContent%22%3A%22%23FFFFFF%22%2C%22neutral%22%3A%22%23FFFFFF%22%2C%22roundedBtn%22%3A%2226px%22%2C%22roundedBox%22%3A%221rem%22%2C%22roundedDropDown%22%3A%220rem%22%2C%22displayDivider%22%3Afalse%7D%2C%22slippage%22%3A1.5%2C%22infiniteApproval%22%3Afalse%2C%22instantExec%22%3Afalse%2C%22apiUrl%22%3A%22https%3A%2F%2Fapi.squidrouter.com%22%2C%22comingSoonChainIds%22%3A%5B421613%2C97%2C4002%2C44787%2C%22osmo-test-4%22%2C%22axelar-testnet-lisbon-3%22%2C%22harpoon-4%22%2C%22atlantic-1%22%2C%22dorado-1%22%2C%22evmos_9000-4%22%2C%22meteor-test%22%2C%22euphoria-1%22%2C42161%2C%22cosmoshub-4%22%2C%22crescent-1%22%2C%22injective-1%22%2C%22juno-1%22%2C%22kaiyo-1%22%2C%22osmosis-1%22%2C%22secret-4%22%2C%22phoenix-1%22%2C%22agoric-3%22%2C%22mantle-1%22%2C%22axelar-dojo-1%22%2C%22comdex-1%22%2C%22evmos_9001-2%22%2C%22fetchhub-4%22%2C%22kichain-2%22%2C%22regen-1%22%2C%22umee-1%22%5D%2C%22titles%22%3A%7B%22swap%22%3A%22Convert%22%2C%22settings%22%3A%22Settings%22%2C%22wallets%22%3A%22Wallets%22%2C%22tokens%22%3A%22Tokens%22%2C%22chains%22%3A%22Chains%22%2C%22history%22%3A%22History%22%2C%22transaction%22%3A%22Transaction%22%2C%22destination%22%3A%22Destination%20address%22%7D%2C%22priceImpactWarnings%22%3A%7B%22warning%22%3A3%2C%22critical%22%3A5%7D%7D"
+    />
+  )
+}
+
 // Route-based code splitting
 const Farms = lazy(() => import('./views/Farms'))
 const Pools = lazy(() => import('./views/Pools'))
@@ -229,7 +246,7 @@ const App: React.FC = () => {
               <Redirect to="/farms" />
             </Route>
             {/* SWAP ROUTES */}
-            <Route path="/swap" component={Swap} />
+            <Route path="/swap" component={NewSwap} />
             <Route exact strict path="/limit-orders" component={Orders} />
             <Route exact strict path="/swap/:outputCurrency" component={RedirectToSwap} />
             <Route exact strict path="/send" component={RedirectPathToSwapOnly} />
